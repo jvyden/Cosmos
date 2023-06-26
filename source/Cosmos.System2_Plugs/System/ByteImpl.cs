29d920fa617bff4ea41a665551758acbe33b18e1
jvyden <jvyden@jvyden.xyz>
2023-06-25 23:21:13 -0400

    Add byte.Parse()

--- source/Cosmos.System2_Plugs/System/ByteImpl.cs
+++ source/Cosmos.System2_Plugs/System/ByteImpl.cs
@@ -9,5 +9,27 @@ namespace Cosmos.System_Plugs.System
         public static string ToString(ref byte aThis) => StringHelper.GetNumberString(aThis);
 
         public static string ToString(ref byte aThis, string format, IFormatProvider provider) => aThis.ToString();
+
+        public static byte Parse(string s)
+        {
+            const string digits = "0123456789";
+            byte result = 0;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                short ind = (short)digits.IndexOf(s[i]);
+                if (ind == -1)
+                {
+                    Console.Write("Digit '");
+                    Console.Write(s[i]);
+                    Console.WriteLine("' not found!");
+                    throw new FormatException();
+                }
+
+                result = (byte)(result * 10 + ind);
+            }
+
+            return result;
+        }
     }
 }
